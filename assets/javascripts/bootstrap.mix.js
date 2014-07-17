2e45db086c69983be6cffbdf5b6c783058ab7c91
Kike Domingo <kike@kikedomingo.com>
2014-07-17 10:33:28 +0100

    First Commit - BTS 3.2
    Bootstrap 3.2

---
+++ assets/javascripts/bootstrap.mix.js
@@ -0,0 +1,11 @@
+@import "bootstrap/affix.js";
+@import "bootstrap/button.js";
+@import "bootstrap/carousel.js";
+@import "bootstrap/collapse.js";
+@import "bootstrap/dropdown.js";
+@import "bootstrap/tab.js";
+@import "bootstrap/transition.js";
+@import "bootstrap/scrollspy.js";
+@import "bootstrap/modal.js";
+@import "bootstrap/tooltip.js";
+@import "bootstrap/popover.js";
